f5f70ddcf47eaa8ef63e80cd98b71c9a10b01977
mango <setsunamango@gmail.com>
2024-01-22 09:56:53 +0800

    feat: 新增useDebounceState

--- src/hooks/debounce.state.tsx
+++ src/hooks/debounce.state.tsx
@@ -1,1 +1,15 @@
-/** TODO */
+import { useEffect, useMemo, useState } from 'react';
+
+import { debounce } from '@busymango/utils';
+
+export function useDebounceState<T = unknown>(state?: T, wait = 300) {
+  const [memo, setMemo] = useState<T | undefined>(state);
+
+  const debounced = useMemo(() => debounce(setMemo, wait), [wait, setMemo]);
+
+  useEffect(() => {
+    debounced.starer(state);
+  }, [state, debounced]);
+
+  return memo;
+}
