e5119228fdd833fcd078a811942aaf9bf04ffdeb
mango <setsunamango@gmail.com>
2024-01-25 17:56:48 +0800

    fix: 处理编译失败的问题

--- src/caniuse.tsx
+++ src/caniuse.tsx
@@ -1,4 +1,4 @@
 /** caniuse-regexp */
 
 export const caniuse =
-  /Chrom(ium|e)\/65\.0(\.\d+|)([\d.]+$|.*Safari\/(?![\d.]+ Edge\/[\d.]+$))/;
+  /Firefox\/(8[89]|9\d|\d{3,})\.\d+(\.\d+|)|Chrom(ium|e)\/(6[6-9]|[7-9]\d|\d{3,})\.\d+(\.\d+|)([\d.]+$|.*Safari\/(?![\d.]+ Edge\/[\d.]+$))/;
