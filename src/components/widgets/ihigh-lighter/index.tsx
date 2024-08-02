ce285fe472f4d8deb776686bf06d2399c8877d81
mango <setsunamango@gmail.com>
2024-08-02 16:18:08 +0800

    feat: 新增 highlighter 组件

--- src/components/widgets/ihigh-lighter/index.tsx
+++ src/components/widgets/ihigh-lighter/index.tsx
@@ -5,9 +5,13 @@ import { iLCSubSeq, iLCSubStr } from '@/utils';
 import styles from './index.scss';
 
 interface LighterProps {
+  /** 目标文本 */
   content: string;
+  /** 需要高亮的关键字 */
   keyword: string;
+  /** 关键字比对方法（判断字符是否需要高亮） */
   compare: typeof Object.is;
+  /** 高亮渲染方式 */
   render: (word: string) => React.ReactNode;
 }
 
@@ -65,6 +69,7 @@ const SeqLighter: React.FC<LighterProps> = (props) => {
 };
 
 export interface IHighLighterProps extends Partial<LighterProps> {
+  /** 比对算法: `str`最长公共子串; `seq`最长公共子序列 */
   mode?: 'str' | 'seq';
 }
 
